ee6293b3f57e4d698c621038746f2e4a6cfd8217
Yu Lin <2604370@sit.singaporetech.edu.sg>
2026-09-04 15:02:14 +0800

    maybe last change

--- hello.py
+++ hello.py
@@ -24,3 +24,19 @@ print("Day 2:", followers)
 
 followers += 10
 print("Day 3:", followers)
+
+
+# Activity 4
+username = input("Enter Username: ")
+age = int(input("Enter Age: "))
+category = input("Enter Content Category: ")
+
+print("\nInstagram Profile")
+print("===============================")
+print("Username:", username)
+print("Age: ", age)
+print("Category: ", category)
+
+# Activity 5
+if age>40 and category == "fun":
+    print("You are old what is fun for you??")
