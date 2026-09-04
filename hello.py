8ad6cc86d3d02937f84828dee013cb4689ace3f1
Yu Lin <2604370@sit.singaporetech.edu.sg>
2026-09-04 14:47:31 +0800

    Update hello.py

--- hello.py
+++ hello.py
@@ -14,3 +14,13 @@ print("Bio:", bio)
 print("Followers:", followers)
 
 # Activity 3
+followers = 100
+
+followers += 50
+print("Day 1:", followers)
+
+followers += 20
+print("Day 2:", followers)
+
+followers += 10
+print("Day 3:", followers)
