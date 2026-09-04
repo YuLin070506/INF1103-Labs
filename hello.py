76cc48a652fbc884a76c9b6a9250968dd84fb40f
Yu Lin <2604370@sit.singaporetech.edu.sg>
2026-09-04 14:42:48 +0800

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,4 +1,16 @@
-print("============================")
+# Activity 1
+print("===============================")
 print("Welcome here")
 print("My first post!")
-print("============================")
+print("===============================")
+
+# Activity 2
+username = "cool_creator"
+bio = "Fun Blogger"
+followers = 100
+
+print("Username:", username)
+print("Bio:", bio)
+print("Followers:", followers)
+
+# Activity 3
